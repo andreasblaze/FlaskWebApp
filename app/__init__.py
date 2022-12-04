02b938c0d8a6dbeb8ed17c21b0f4323d5eade27b
Andrii Bondariev <andrii.bondariev@namecheap.com>
2022-12-04 20:33:57 +0200

    feat: added sensors

--- app/__init__.py
+++ app/__init__.py
@@ -15,9 +15,11 @@ def create_app():
 
     from .views import views
     from .auth import auth
+    from .sensors import sensors
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(sensors, url_prefix='/')
 
     from .models import User, Note
 
