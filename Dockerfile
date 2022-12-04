62c59145009269dfd8d61805e25d98cc072e892c
Andrii Bondariev <andrii.bondariev@namecheap.com>
2022-12-04 16:10:51 +0200

    feat: added sensors and updated d-file

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM python:3.9.7-alpine3.14
+FROM python:buster 
 ENV PYTHONUNBUFFERED 1
 
 LABEL VERSION="1.0.0"
-LABEL MAINTAINER="Andrey Bondariev"
-LABEL EMAIL="andreybond13@gmail.com"
+LABEL MAINTAINER="Andrii Bondariev"
+LABEL EMAIL="andrii.bondariev@nure.ua"
 LABEL NAME="Flask Web Application"
 
 WORKDIR /FlaskWebApp
@@ -15,6 +15,6 @@ RUN pip install -r requirements.txt
 
 COPY ./app ./app
 
-EXPOSE 80
+EXPOSE 5000
 
-CMD [ "python3", "./main.py" ]
+ENTRYPOINT [ "python", "main.py" ]
--- app/views.py
+++ app/views.py
@@ -14,12 +14,12 @@ def home():
         note = request.form.get('note')
 
         if len(note) < 1:
-            flash('Note is too short!', category='error')
+            flash('Not valid sensor!', category='error')
         else:
             new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-            flash('Note added!', category='success')
+            flash('Sensor added!', category='success')
 
     return render_template("home.html", user=current_user)
 
